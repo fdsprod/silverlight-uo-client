45f91ea59c4d392b18efb3cc94ab355ec0ef8a00
fdsprod@gmail.com <fdsprod@gmail.com@6419d475-cb8f-69b5-42a4-2fde9c9f03b1>
2011-10-13 19:23:12 +0000

    Cleanup

---
+++ client/Client/Client/Network/Packets/LoginPacket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Client.Network.Packets
+{
+    public class LoginPacket : Packet
+    {
+        public LoginPacket(string username, string password)
+            : base(0x80, 62)
+        {
+            Stream.WriteAsciiFixed(username, 30);
+            Stream.WriteAsciiFixed(password, 30);
+            Stream.Write((byte)0x5D);
+        }
+    }
+}
---
+++ client/Client/Client/Network/Packets/SeedPacket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Client.Network.Packets
+{
+    public class SeedPacket : Packet
+    {
+        public SeedPacket() :
+            base(0x01, 4)
+        {
+            Stream.Write(new byte[] { 0x02, 0x03, 0x04 }, 0, 3);
+        }
+    }
+}
